c6d29ccab50937c317defae67a1a2f719bd0bf8a
CassianoFerreira <cassianoferreira10@gmail.com>
2019-11-20 19:58:43 +0000

    Criado a classe address

---
+++ src/main/java/address.java
@@ -0,0 +1,28 @@
+public class address {
+    private String streetname;
+    private String doorno;
+    private String postalcode;
+
+
+    public String getStreetname (){
+      return streetname;
+    }
+    public String getDoorno (){
+        return doorno;
+    }
+    public String getPostalcode (){
+        return postalcode;
+    }
+
+    public void setStreetname(String streetname) {
+        this.streetname = streetname;
+    }
+
+    public void setDoorno(String doorno) {
+        this.doorno = doorno;
+    }
+
+    public void setPostalcode(String postalcode) {
+        this.postalcode = postalcode;
+    }
+}
