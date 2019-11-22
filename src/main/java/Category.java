a1e705889ebc7d3d3267badea7dc4391c5ee7a68
Ricardocoutinhok <coutinho7947@hotmail.com>
2019-11-22 19:57:27 +0000

    Criado construtor Category

--- src/main/java/Category.java
+++ src/main/java/Category.java
@@ -3,6 +3,12 @@ public class Category {
     private String description;
     private String color;
 
+    public Category(String name, String description, String color){
+        this.name= name;
+        this.description=description;
+        this.color=color;
+    }
+
     public String getName() {
         return name;
     }
